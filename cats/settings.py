a4ffdc1c13dfeea6d4721665b80fa48d37aaf30e
BrunnenGrazumo <max.razum.mr2@gmail.com>
2025-02-16 09:18:51 +0300

    исправлена проблема с мерцанием шрифтов

--- cats/settings.py
+++ cats/settings.py
@@ -77,6 +77,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
